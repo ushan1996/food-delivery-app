a7ac1014df3b4dd3d22a1d54fd8cf8d48b48a47c
ushan1996 <64035537+ushan1996@users.noreply.github.com>
2021-09-26 22:11:54 +0530

    asset constent

---
+++ lib/utils/constent.dart
@@ -0,0 +1,9 @@
+class Constants {
+  //assets paths
+  static const IMAGE_PATH = 'assets/images/';
+  static const ICON_PATH = 'assets/images/';
+  //asset function
+  static String imageAsset(img) => "$IMAGE_PATH$img";
+  //icon asset function
+  static String iconAsset(img) => "$ICON_PATH$img";
+}
